b600e82f098119324b0a73fd03a5f61ca3d48828
Suraj Iyer <suraj.iyer96@outlook.com>
2016-04-28 15:08:17 +0200

    Integrated with new ionic interface

---
+++ client/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('pollsCtrl', function($scope) {
+
+})
+   
+.controller('practicalitiesCtrl', function($scope) {
+
+})
+   
+.controller('sponsoringCtrl', function($scope) {
+
+})
+      
+.controller('settingsCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+ 
---
+++ client/js/routes.js
@@ -0,0 +1,58 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider.state('menu.polls', {
+    url: '/polls',
+    views: {
+      'side-menu21': {
+        templateUrl: 'client/views/polls.ng.html',
+        controller: 'pollsCtrl'
+      }
+    }
+  })
+
+  .state('menu.practicalities', {
+    url: '/practicalities',
+    views: {
+      'side-menu21': {
+        templateUrl: 'client/views/practicalities.ng.html',
+        controller: 'practicalitiesCtrl'
+      }
+    }
+  })
+
+  .state('sponsoring', {
+    url: '/sponsoring',
+    templateUrl: 'client/views/sponsoring.ng.html',
+    controller: 'sponsoringCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'client/views/menu.ng.html',
+    abstract:true
+  })
+
+  .state('menu.settings', {
+    url: '/settings',
+    views: {
+      'side-menu21': {
+        templateUrl: 'client/views/settings.ng.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'client/views/login.ng.html',
+    controller: 'loginCtrl'
+  })
+
+  $urlRouterProvider.otherwise('/page5')
+});
