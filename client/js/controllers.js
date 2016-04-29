cd0417ff39f7c84d1f0a3caf63a9f85507eb48a6
Suraj Iyer <suraj.iyer96@outlook.com>
2016-04-29 15:05:33 +0200

    Trying to add tabs

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,94 +1,94 @@
 angular.module('app.controllers', [])
-  
-.controller('pollsCtrl', function($scope) {
 
-})
-   
-.controller('practicalitiesCtrl', function($scope) {
+    .controller('pollsCtrl', function ($scope) {
 
-})
-   
-.controller('sponsoringCtrl', function($scope) {
+    })
 
-})
-      
-.controller('settingsCtrl', function($scope) {
+    .controller('practicalitiesCtrl', function ($scope) {
 
-})
-   
-.controller('loginCtrl', function($scope) {
+    })
 
-})
+    .controller('sponsoringCtrl', function ($scope) {
 
-.controller('feedCtrl', function($scope,$state,$stateParams,$ionicModal,$meteor,$ionicPopover) {
-    $scope.posts = $meteor.collection(Posts);
-    
-    /* Post */
+    })
 
-    $scope.newPost={};
+    .controller('settingsCtrl', function ($scope) {
 
-    $scope.post = function () {
-        $scope.newPost.type = '1';
-        $scope.posts.push( $scope.newPost );
-        $scope.newPost={};
-        $scope.closePost();
-    };
+    })
 
-    $ionicModal.fromTemplateUrl('client/views/feeditems/newpost.ng.html', {
-        scope: $scope
-    }).then(function(postmodal) {
-        $scope.postmodal = postmodal;
-    });
+    .controller('loginCtrl', function ($scope) {
 
-    $scope.closePost = function() {
-        $scope.postmodal.hide();
-    };
+    })
 
-    $scope.openPost = function() {
-        $scope.postmodal.show();
-    };
+    .controller('feedCtrl', function ($scope, $state, $stateParams, $ionicModal, $meteor, $ionicPopover) {
+        $scope.posts = $meteor.collection(Posts);
 
-    /* Practicality*/
+        /* Post */
 
-    $scope.newPracticality={};
+        $scope.newPost = {};
 
-    $scope.practicality = function () {
-        $scope.newPracticality.type = '2';
-        $scope.posts.push( $scope.newPracticality );
-        $scope.newPracticality={};
-        $scope.closePracticality();
-    };
+        $scope.post = function () {
+            $scope.newPost.type = '1';
+            $scope.posts.push($scope.newPost);
+            $scope.newPost = {};
+            $scope.closePost();
+        };
 
-    $ionicModal.fromTemplateUrl('client/views/feeditems/practicality.ng.html', {
-        scope: $scope
-    }).then(function(practicalityModal) {
-        $scope.practicalityModal = practicalityModal;
-    });
+        $ionicModal.fromTemplateUrl('client/views/feeditems/newpost.ng.html', {
+            scope: $scope
+        }).then(function (postmodal) {
+            $scope.postmodal = postmodal;
+        });
 
-    $scope.closePracticality = function() {
-        $scope.practicalityModal.hide();
-    };
+        $scope.closePost = function () {
+            $scope.postmodal.hide();
+        };
 
-    $scope.openPracticality = function() {
-        $scope.practicalityModal.show();
-    };
+        $scope.openPost = function () {
+            $scope.postmodal.show();
+        };
+
+        /* Practicality*/
+
+        $scope.newPracticality = {};
+
+        $scope.practicality = function () {
+            $scope.newPracticality.type = '2';
+            $scope.posts.push($scope.newPracticality);
+            $scope.newPracticality = {};
+            $scope.closePracticality();
+        };
+
+        $ionicModal.fromTemplateUrl('client/views/feeditems/practicality.ng.html', {
+            scope: $scope
+        }).then(function (practicalityModal) {
+            $scope.practicalityModal = practicalityModal;
+        });
+
+        $scope.closePracticality = function () {
+            $scope.practicalityModal.hide();
+        };
+
+        $scope.openPracticality = function () {
+            $scope.practicalityModal.show();
+        };
 
 
-    /* POPOVER */
-    $ionicPopover.fromTemplateUrl('client/views/popover.ng.html', {
-        scope: $scope
-    }).then(function(popover) {
-        $scope.popover = popover;
-    });
-    $scope.openPopover = function($event) {
-        $scope.popover.show($event);
-    };
-    $scope.closePopover = function() {
-        $scope.popover.hide();
-    };
-    //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function() {
-        $scope.popover.remove();
-    });
+        /* POPOVER */
+        $ionicPopover.fromTemplateUrl('client/views/templates/popover.ng.html', {
+            scope: $scope
+        }).then(function (popover) {
+            $scope.popover = popover;
+        });
+        $scope.openPopover = function ($event) {
+            $scope.popover.show($event);
+        };
+        $scope.closePopover = function () {
+            $scope.popover.hide();
+        };
+        //Cleanup the popover when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.popover.remove();
+        });
 
-})
+    })
