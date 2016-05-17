d19ea8e975557e085494bba007c72e3facd82140
Jarno <jarnoverhagen@gmail.com>
2016-05-17 14:40:40 +0200

    restructured controller test

--- client/app/js/controllers.tests.js
+++ client/app/js/controllers.tests.js
@@ -7,30 +7,9 @@ import './services.js'
 import './routes.js'
 import '/model/Feed'
 
-describe('feedCtrl', () => {
-    var scope, ctrl;
-
-    beforeEach(angular.mock.module('angular-meteor'));
-    beforeEach(angular.mock.module('app.services'));
-    beforeEach(angular.mock.module('app.controllers'));
-    beforeEach(inject(($rootScope, $controller, _CoachAccess_) => {
-        scope = $rootScope;
-        ctrl = $controller('feedCtrl', {
-            $scope: scope,
-            CoachAccess: _CoachAccess_
-        });
-    }));
-
-    it('should create 4 feed item types', () => {
-        assert.isFalse(scope.showFilter);
-        scope.showFilter = true;
-        assert.isTrue(scope.showFilter);
-    });
-});
-
-describe('registerCtrl', () => {
-    var scope, meteor, state, ctrl;
-
+var scope, meteor, state, ctrl;
+    
+function setupTesting(ctrlName) {
     beforeEach(angular.mock.module('angular-meteor'));
     beforeEach(angular.mock.module('app.services'));
     beforeEach(angular.mock.module('app.controllers'));
@@ -41,17 +20,22 @@ describe('registerCtrl', () => {
         meteor = $meteor;
         state = $state;      
         
-        ctrl = $controller('registerCtrl', {
+        ctrl = $controller(ctrlName, {
             $scope: scope,
             $meteor: meteor,
             $state: state
         });
     }));
+}
+
+describe('registerCtrl', () => {
+    setupTesting('registerCtrl');
 
     it("Should register a user", (done) => {
         email = 'test' + new Date().getTime() + '@test.test';
         password = 'password';
         
+        Meteor.logout();
         assert(Meteor.userId() == null, 'User is not logged in.');
         scope.user.email = email;
         scope.user.password = password;
