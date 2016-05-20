f313a5110e3f11d2300f2ca85255f42b0ada744b
Daniël van der Laan <daniel.vdlaan@gmail.com>
2016-05-20 12:43:35 +0200

    Comments

--- client/web/js/routes.js
+++ client/web/js/routes.js
@@ -5,11 +5,17 @@ angular.module('web.routes', [])
         // Learn more here: https://github.com/angular-ui/ui-router
         // Set up the various states which the app can be in.
         // Each state's controller can be found in controllers.js
-        
+
+        /*
+        *       Routes for the web interface
+        */
+
         $stateProvider
             .state('/', {
                 url: '/',
                 controller: function ($scope, $state) {
+
+                    // If user logged in
                     if (Meteor.userId()) {
                         $state.go('web.feed');
                     } else {
@@ -17,60 +23,71 @@ angular.module('web.routes', [])
                     }
                 }
             })
+
+            // Just the menu view
             .state('web', {
                 url: '/web',
                 templateUrl: 'client/web/views/menu.ng.html',
                 controller: 'mainController'
             })
 
+            // Main
             .state('main', {
                 url: '/main',
                 templateUrl: 'client/web/views/main.ng.html',
                 controller: 'mainController'
             })
 
+            // Login
             .state('login', {
                 url: '/login',
                 templateUrl: 'client/web/views/login.ng.html',
                 controller: 'loginController'
             })
 
+            // Adding members
             .state('web.addAccount', {
                 url: '/addAccount',
                 templateUrl: 'client/web/views/addAccount.ng.html',
                 controller: 'addAccountController'
             })
 
+            // Member management
             .state('web.members', {
                 url: '/members',
                 templateUrl: 'client/web/views/members.ng.html',
                 controller: 'accountManagementCtrl'
             })
 
+            // Club settings
             .state('web.settings', {
                 url: '/settings',
                 templateUrl: 'client/web/views/settings.ng.html',
                 controller: 'accountManagementCtrl'
             })
 
+            // User profile
             .state('web.profile', {
                 url: '/profile',
                 templateUrl: 'client/web/views/profile.ng.html'
             })
 
+            // Feed
             .state('web.feed', {
                 url: '/feed',
                 templateUrl: 'client/web/views/feed.ng.html'
             })
 
+            // Club betting
             .state('web.betting', {
                 url: '/betting',
                 templateUrl: 'client/web/views/betting.ng.html',
                 controller: 'accountManagementCtrl'
             })
 
-            .state('sepQuotes', {
-                url: '/sepQuotes',
+            // Extra: quotes
+            .state('web.sepquotes', {
+                url: '/sepquotes',
                 templateUrl: 'client/web/views/sepquotes.ng.html'
             });
         
