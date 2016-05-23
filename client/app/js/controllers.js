b38bd9cd15823c98f6a1d9f76baf8b5369e08ebc
Suraj Iyer <suraj.iyer96@outlook.com>
2016-05-23 23:17:57 +0200

    Aesthetic changes

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -2,75 +2,6 @@ import Moment from 'moment';
 
 angular.module('app.controllers', [])
 
-
-     .controller('chatsCtrl', function ($scope, $state, $stateParams) {
-
-        //  $scope.helpers({
-        //       data() {
-        //         return Chats.find();
-        //       }
-        //     });
-        // }
-
-    //     $scope.chats = [
-    //   {
-    //     _id: 0,
-    //     name: 'Ethan Gonzalez',
-    //     picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-    //     lastMessage: {
-    //       text: 'You on your way?',
-    //       timestamp: Moment().subtract(1, 'hours').toDate()
-    //     }
-    //   },
-    //   {
-    //     _id: 1,
-    //     name: 'Bryan Wallace',
-    //     picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-    //     lastMessage: {
-    //       text: 'Hey, it\'s me',
-    //       timestamp: Moment().subtract(2, 'hours').toDate()
-    //     }
-    //   },
-    //   {
-    //     _id: 2,
-    //     name: 'Avery Stewart',
-    //     picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-    //     lastMessage: {
-    //       text: 'I should buy a boat',
-    //       timestamp: Moment().subtract(1, 'days').toDate()
-    //     }
-    //   },
-    //   {
-    //     _id: 3,
-    //     name: 'Katie Peterson',
-    //     picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-    //     lastMessage: {
-    //       text: 'Look at my mukluks!',
-    //       timestamp: Moment().subtract(4, 'days').toDate()
-    //     }
-    //   },
-    //   {
-    //     _id: 4,
-    //     name: 'Ray Edwards',
-    //     picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-    //     lastMessage: {
-    //       text: 'This is wicked good ice cream.',
-    //       timestamp: Moment().subtract(2, 'weeks').toDate()
-    //     }
-    //   }
-    // ];
-      
-        Meteor.subscribe('Chats');
-
-        $scope.helpers({
-            chats: function () {
-                return Chats.find();
-            }
-        });
-
-
-    })
-
     .controller('ItemCtrl', function ($scope) {
         if (!$scope.item) {
             throw new Error("No item object passed.");
@@ -194,10 +125,6 @@ angular.module('app.controllers', [])
         };
     })
 
-    .controller('chatCtrl', function($scope) {
-        
-    })
-
     .controller('feedCtrl', function ($scope, AccessControl) {
         // Display coach bar
         AccessControl.getPermission('CoachBar', 'view', function (result) {
@@ -234,8 +161,8 @@ angular.module('app.controllers', [])
         $scope.openDetails = function () {
             console.log("poep");
         };
-        
-        
+
+
         // Subscribe to the feed
         // $scope.subscribe('Feed', function(){
         //     console.log($scope.getCollectionReactively('itemTypesFilter'));
@@ -273,6 +200,72 @@ angular.module('app.controllers', [])
         //Cleanup the popover when we're done with it!
         $scope.$on('$destroy', function () {
             $scope.popover.remove();
+        });
+    })
+
+    .controller('chatsCtrl', function ($scope, $state, $stateParams) {
+
+        //  $scope.helpers({
+        //       data() {
+        //         return Chats.find();
+        //       }
+        //     });
+        // }
+
+        //     $scope.chats = [
+        //   {
+        //     _id: 0,
+        //     name: 'Ethan Gonzalez',
+        //     picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+        //     lastMessage: {
+        //       text: 'You on your way?',
+        //       timestamp: Moment().subtract(1, 'hours').toDate()
+        //     }
+        //   },
+        //   {
+        //     _id: 1,
+        //     name: 'Bryan Wallace',
+        //     picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
+        //     lastMessage: {
+        //       text: 'Hey, it\'s me',
+        //       timestamp: Moment().subtract(2, 'hours').toDate()
+        //     }
+        //   },
+        //   {
+        //     _id: 2,
+        //     name: 'Avery Stewart',
+        //     picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
+        //     lastMessage: {
+        //       text: 'I should buy a boat',
+        //       timestamp: Moment().subtract(1, 'days').toDate()
+        //     }
+        //   },
+        //   {
+        //     _id: 3,
+        //     name: 'Katie Peterson',
+        //     picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
+        //     lastMessage: {
+        //       text: 'Look at my mukluks!',
+        //       timestamp: Moment().subtract(4, 'days').toDate()
+        //     }
+        //   },
+        //   {
+        //     _id: 4,
+        //     name: 'Ray Edwards',
+        //     picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
+        //     lastMessage: {
+        //       text: 'This is wicked good ice cream.',
+        //       timestamp: Moment().subtract(2, 'weeks').toDate()
+        //     }
+        //   }
+        // ];
+
+        Meteor.subscribe('Chats');
+
+        $scope.helpers({
+            chats: function () {
+                return Chats.find();
+            }
         });
     })
 
@@ -489,10 +482,6 @@ angular.module('app.controllers', [])
         $scope.openHero = function () {
             $scope.heromodal.show();
         };
-    })
-
-    .controller('pollsCtrl', function ($scope) {
-
     })
 
     .controller('settingsCtrl', function ($scope) {
