88e0eefa62638bc945d6086ab6e23b4a41f55abb
jgcbrouns <j.brouns@home.nl>
2016-05-26 12:40:04 -0700

    Added remote database comments

--- server/AccessControl.js
+++ server/AccessControl.js
@@ -1,40 +1,40 @@
-// import {isAdmin} from '/imports/common';
-// import {accessControlSchema} from '/imports/schemas/misc';
+import {isAdmin} from '/imports/common';
+import {accessControlSchema} from '/imports/schemas/misc';
 
-// AMx = new Mongo.Collection("AccessControl");
+AMx = new Mongo.Collection("AccessControl");
 
-// Meteor.startup(function () {
-//     AMx.attachSchema(accessControlSchema);
-//     console.log(process.env.MONGO_URL);
-// });
+Meteor.startup(function () {
+    AMx.attachSchema(accessControlSchema);
+    console.log(process.env.MONGO_URL);
+});
 
-// Meteor.methods({
-//     /**
-//      * Check user permissions (Access control).
-//      * @param itemType the item for which permission is being requested
-//      * @param permission the type of permission being requested for the item: create, view, edit or delete.
-//      * @returns true if permitted or false if denied
-//      */
-//     checkRights: function (itemType, permission) {
-//         check(itemType, String);
-//         check(permission, String);
+Meteor.methods({
+    /**
+     * Check user permissions (Access control).
+     * @param itemType the item for which permission is being requested
+     * @param permission the type of permission being requested for the item: create, view, edit or delete.
+     * @returns true if permitted or false if denied
+     */
+    checkRights: function (itemType, permission) {
+        check(itemType, String);
+        check(permission, String);
 
-//         var doc = AMx.findOne(
-//             {'_id': Meteor.call('getUserType')},
-//             {fields: {'items': {$elemMatch: {'_id': itemType}}}}
-//         );
+        var doc = AMx.findOne(
+            {'_id': Meteor.call('getUserType')},
+            {fields: {'items': {$elemMatch: {'_id': itemType}}}}
+        );
 
-//         if(!doc.items) {
-//             throw new Meteor.Error('No such permission defined');
-//         }
+        if(!doc.items) {
+            throw new Meteor.Error('No such permission defined');
+        }
 
-//         return doc.items[0].permissions[permission];
-//     },
-//     setPermissions: function (newAccess) {
-//         check(this.userId, Match.Where(isAdmin));
-//         check(newAccess, accessControlSchema);
-//         AMx.insert(newAccess, function (err) {
-//             if (err) throw new Meteor.Error(err.error);
-//         });
-//     }
-// });
+        return doc.items[0].permissions[permission];
+    },
+    setPermissions: function (newAccess) {
+        check(this.userId, Match.Where(isAdmin));
+        check(newAccess, accessControlSchema);
+        AMx.insert(newAccess, function (err) {
+            if (err) throw new Meteor.Error(err.error);
+        });
+    }
+});
--- server/lib/config.js
+++ server/lib/config.js
@@ -1,8 +1,13 @@
 Meteor.startup(function() {
-	process.env.MONGO_URL = "mongodb://sepsep:36RDIOM17d0s9Z6K-K4N@clubnet-9079.mongo.dbs.appsdeck.eu/clubnet-9079";
+	process.env.MONGO_URL = "mongodb://clubnet-9079:Zqz2HTvk7VX8bujl_YHv@clubnet-9079.mongo.dbs.appsdeck.eu:30065/clubnet-9079";
 });
 
-//mongorestore -u "sepsep" -p "36RDIOM17d0s9Z6K-K4N" -h "clubnet-9079.mongo.dbs.appsdeck.eu/clubnet-9079:3001"
-//mongodb://clubnet-9079:Zqz2HTvk7VX8bujl_YHv@clubnet-9079.mongo.dbs.appsdeck.eu:30065/clubnet-9079
+MONGO_URL = "mongodb://clubnet-9079:Zqz2HTvk7VX8bujl_YHv@clubnet-9079.mongo.dbs.appsdeck.eu:30065/clubnet-9079";
+//mongorestore -u "sepsep" -p "36RDIOM17d0s9Z6K-K4N" -h "clubnet-9079.mongo.dbs.appsdeck.eu/clubnet-9079:3001"//mongodb://clubnet-9079:Zqz2HTvk7VX8bujl_YHv@clubnet-9079.mongo.dbs.appsdeck.eu:30065/clubnet-9079
+
+//mongorestore -u clubnet-9079 -p  Zqz2HTvk7VX8bujl_YHv -h 127.0.0.1:10000 -d clubnet-9079 dump/meteor
+//mongorestore -u clubnet-9079 -p Zqz2HTvk7VX8bujl_YHv -h clubnet-9079.mongo.dbs.appsdeck.eu:30065 -d clubnet-9079 ../../meteor
 //git push scalingo master
+
+
 
