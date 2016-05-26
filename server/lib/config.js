10c434c63bdfa896eb80570e93d1ea7f9b40a10b
jgcbrouns <j.brouns@home.nl>
2016-05-26 11:55:20 -0700

    test

---
+++ server/lib/config.js
@@ -0,0 +1,8 @@
+Meteor.startup(function() {
+	process.env.MONGO_URL = "mongodb://sepsep:36RDIOM17d0s9Z6K-K4N@clubnet-9079.mongo.dbs.appsdeck.eu/clubnet-9079";
+});
+
+//mongorestore -u "sepsep" -p "36RDIOM17d0s9Z6K-K4N" -h "clubnet-9079.mongo.dbs.appsdeck.eu/clubnet-9079:3001"
+//mongodb://clubnet-9079:Zqz2HTvk7VX8bujl_YHv@clubnet-9079.mongo.dbs.appsdeck.eu:30065/clubnet-9079
+//git push scalingo master
+
