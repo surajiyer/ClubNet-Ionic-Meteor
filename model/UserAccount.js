c7070ed10aeaf2da9e30276b78b0d88aa2b1ccc0
Daniël van der Laan <daniel.vdlaan@gmail.com>
2016-05-26 12:43:07 +0200

    Enrollment Email
    To do: create enrollment page

--- model/UserAccount.js
+++ model/UserAccount.js
@@ -51,11 +51,6 @@ if(Meteor.isServer) {
                 password: String,
                 profile: userProfileSchema
             });
-            // try {
-                return Accounts.createUser(newUser);
-            // } catch (err) {
-            //     return err;
-            // }
         },
         updateUserProfile: function (newInfo) {
             // TODO: should not check full user profile schema for update
