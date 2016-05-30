f16614018cbf982860b35e59fb36743513a90b81
Kevin <k.v.eenige@student.tue.nl>
2016-05-30 12:32:30 +0200

    Tests + basis for commenting

--- model/UserAccount.js
+++ model/UserAccount.js
@@ -2,9 +2,14 @@ import {isAdmin} from '/imports/common';
 import {userSchema, userProfileSchema} from '/imports/schemas/users';
 import {notesSchema} from '/imports/schemas/misc';
 import {Meteor} from 'meteor/meteor';
-
+/**
+ * @summary Rules and Methods for the users collection.
+ * On startup it will set the deny and allow rules, publish the user data and attach the userSchema
+ * @param {Object} userSchema The schema that will the attached to the users collection.
+ */
 Meteor.startup(function () {
     // Set deny rules
+    // Except admins, nobody is allowed insertion, deletion and removal
     Meteor.users.deny({
         insert: function (userId) {
             return !isAdmin(userId);
@@ -18,6 +23,7 @@ Meteor.startup(function () {
     });
 
     // Set allow rules
+    // Except admins, nobody is allowed insertion, deletion and removal
     Meteor.users.allow({
         insert: function (userId) {
             return isAdmin(userId);
@@ -30,7 +36,7 @@ Meteor.startup(function () {
         }
     });
 
-    // Publish userData
+    // If the user that is logged in is an admin user, publish all users in the collection.
     if (Meteor.isServer) {
         Meteor.publish("userData", function () {
                 if (isAdmin(this.userId))
@@ -47,15 +53,25 @@ Meteor.startup(function () {
 if(Meteor.isServer) {
     process.env.MAIL_URL="smtp://clubnet.noreply%40gmail.com:y4VP3Hq2Lvbs@smtp.gmail.com:587/"; 
     Meteor.methods({
+        /**
+         * @summary Function for adding a new user to the collection
+         * It will check whether or not the new user adheres to the schema.
+         * If so, it will add the user to the collection and it will send an enrollment email to the new user.
+         * @param {Object} newUser An object that wants to be added to the collection, needs to adhere to the schema.
+         * @returns {String} userId The id of the newly created user.
+         */
         addUser: function (newUser) {
+            // Validate the information in the newUser.
             check(newUser, {
                 email: String,
                 password: String,
                 profile: userProfileSchema
             });
+            // Add the user to the collection
+            userId = Accounts.createUser(newUser);
+            // Create an email template
             credPassword = newUser.password;
             credEmail = newUser.email;
-            userId = Accounts.createUser(newUser);
             Accounts.emailTemplates.siteName = "ClubNet";
             Accounts.emailTemplates.from = "ClubNet <accounts@example.com>";
             Accounts.emailTemplates.enrollAccount.subject = function (newUser) {
@@ -79,9 +95,17 @@ if(Meteor.isServer) {
                     + "The ClubNet team.\n\n" +
                     url;
             };
+            // Send the email
             Accounts.sendEnrollmentEmail(userId);
             return userId;
         },
+        /**
+         * @summary Function for updating the information of a certain user.
+         * It will first check whether the parameters are valid.
+         * If so, it will update the user with the new information
+         * @param {String} userID The id of the user to have its information be updated
+         * @param {Object} newInfo The new information to be added to the user.
+         */
         updateUserProfile: function (userID, newInfo) {
             // TODO: should not check full user profile schema for update
             check(userID, String);
@@ -91,16 +115,34 @@ if(Meteor.isServer) {
                 {$set: {profile: newInfo}}
             );
         },
+        /**
+         * @summary Function for getting the information of a certain user.
+         * This can only be done by a user that had admin level rights.
+         * It will first check whether the parameters are valid and if the user is an admin.
+         * If so, it will try to get the information.
+         * @param {String} userID The id of the user who's information needs to be retrieved.
+         */
         getUserInfo: function (userID) {
             check(userID, String);
             check(Meteor.userId(), Match.Where(isAdmin));
             return Meteor.users.find({_id: userID}).fetch()[0];
         },
+        /**
+         * @summary Function for getting the type of the current logged in user.
+         * It will first check whether the parameters are valid.
+         * If so, it will try to get the type of the user.
+         */
         getUserType: function () {
             check(Meteor.userId(), String);
             //return Meteor.users.find({_id: this.userId}).fetch()[0].profile.type;
             return Meteor.user().profile.type;
         },
+        /**
+         * @summary Function for adding a note.
+         * It will first check whether the parameters adhere to the schema.
+         * If so, it will store the note as a part of the user.
+         * @param newNote The note that needs to be added.
+         */
         addNote: function (newNote) {
             check(newNote, notesSchema);
             Meteor.users.update(
@@ -108,6 +150,12 @@ if(Meteor.isServer) {
                 {$push: {'notes': newNote}}
             );
         },
+        /**
+         * @summary Function for updating a note.
+         * It will first check whether the parameters adhere to the schema.
+         * If so, it will find the note and update the text.
+         * @param newNote The note that needs to be updated, but with different text.
+         */
         updateNote: function (newNote) {
             check(newNote, notesSchema);
             Meteor.users.update(
