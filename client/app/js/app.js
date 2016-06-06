d4f383cb30485ab36db52c2940daf54265b480ae
jgcbrouns <j.brouns@home.nl>
2016-06-06 11:02:16 -0700

    Deployed version

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -35,10 +35,5 @@ function onReady() {
     angular.bootstrap(document, ['app']);
 }
 
-// TODO: Remove else condition in production
-if (Meteor.isCordova) {
+
     angular.element(document).on("deviceready", onReady);
-}
-else {
-    angular.element(document).ready(onReady);
-}
