749909766d578a8752b74f20aa3ab94228052168
s138308 <somiciuss@gmail.com>
2016-06-07 15:32:12 +0200

    my boobs are C++  -> INFINITE SCROLL

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -33,9 +33,5 @@ function onReady() {
     angular.bootstrap(document, ['app']);
 }
 
-if(Meteor.isCordova) {
     angular.element(document).on("deviceready", onReady);
-}
-else {
-    angular.element(document).ready(onReady);
-}
+
