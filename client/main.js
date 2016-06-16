1ec093e3ae48dfcc52b08e89022316f6b2c7ec8d
s138308 <somiciuss@gmail.com>
2016-06-16 15:59:08 +0200

    loadApp both places

--- client/main.js
+++ client/main.js
@@ -28,7 +28,7 @@ if(Meteor.isProduction) {
     if (Meteor.isCordova) {
         angular.element(document).on("deviceready", loadApp);
     } else {
-        angular.element(document).ready(loadWeb);
+        angular.element(document).ready(loadApp);
     }
 } else {
     angular.element(document).ready(loadWeb);
